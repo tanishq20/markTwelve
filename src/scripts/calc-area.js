4ea6be98bbe40de63afa149419b63bc16e063e6b
Tanishq Kumar <66270248+tanishq20@users.noreply.github.com>
2021-09-20 21:38:20 +0530

    fix: area negative value handled

--- src/scripts/calc-area.js
+++ src/scripts/calc-area.js
@@ -1,16 +1,15 @@
-const base = document.querySelector('#base')
-const height = document.querySelector('#height')
+const sides = document.querySelectorAll('.sides')
 const showBtn = document.querySelector('#calc-btn')
 const output = document.querySelector('#output')
 
 showBtn.addEventListener('click', () => {
-  const baseValue = Number(base.value)
-  const heightValue = Number(height.value)
-  const area = (baseValue * heightValue) / 2
-  if (baseValue === 0 || heightValue === 0) {
-    showMessage('Please fill all the fileds with positive value🙁', 'red')
+  const baseValue = Number(sides[0].value)
+  const heightValue = Number(sides[1].value)
+  if (baseValue > 0 && heightValue > 0) {
+    const area = (baseValue * heightValue) / 2
+    showMessage(`The area of the triangle is ${area.toFixed(2)} cm²`, 'green')
   } else {
-    showMessage(`The area of the triangle is ${area}`, 'orange')
+    showMessage('Please fill all the fileds with positive value🙁', 'red')
   }
 })
 
